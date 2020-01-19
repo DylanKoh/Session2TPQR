fa31f4ead2fe61c1394124d3eefd8c27ffc7c8a8
KOH WEI LONG DYLAN TP <1907446G@student.tp.edu.sg>
2020-01-19 23:41:52 +0800

    2.6 Documented

--- 2.6_Update.cs
+++ 2.6_Update.cs
@@ -19,6 +19,7 @@ namespace Session2
             _userID = userID;
         }
 
+        //Redirects User back to Sponsorship Main Menu - 2.3
         private void backBtn_Click(object sender, EventArgs e)
         {
             this.Hide();
@@ -30,6 +31,9 @@ namespace Session2
         {
             GridRefresh();
             var total = 0;
+
+            ///For each row of the DGV, add on total value opf cost of booked (approved) sponsorship package to total and thus to
+            ///the label
             foreach (DataGridViewRow item in dataGridView1.Rows)
             {
                 total += Convert.ToInt32(item.Cells[4].Value);
@@ -38,6 +42,9 @@ namespace Session2
             totalLbl.Text += total.ToString();
         }
 
+        /// <summary>
+        /// This method refreshes the DGV
+        /// </summary>
         private void GridRefresh()
         {
             dataGridView1.ColumnCount = 6;
@@ -70,8 +77,14 @@ namespace Session2
             }
         }
 
+        /// <summary>
+        /// This method is triggered when the delete button is clicked
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void deleteBtn_Click(object sender, EventArgs e)
         {
+            //Checks if there is any row selected at all
             if (dataGridView1.CurrentRow == null)
             {
                 MessageBox.Show("Please select a booking to delete!", "No booking selected",
@@ -79,6 +92,7 @@ namespace Session2
             }
             else
             {
+                //Prompts user with warning message. If yes, then proceeds to delete records of bookings
                 var dl = MessageBox.Show("Are you sure you want to delete this booking?", "Delete",
                     MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
                 if (DialogResult.Yes == dl)
@@ -107,8 +121,14 @@ namespace Session2
             }
         }
 
+        /// <summary>
+        /// Triggered when update button is clicked
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void updateBtn_Click(object sender, EventArgs e)
         {
+            //Check if quantity to update is valid
             if (Int32.Parse(quantityBox.Text) <= 0)
             {
                 MessageBox.Show("Quantity cannot be less than or equals to 0!", "Invalid Amount!",
@@ -118,6 +138,7 @@ namespace Session2
             {
                 using (var context = new Session2Entities1())
                 {
+                    //For each selected rows...
                     foreach (DataGridViewRow item in dataGridView1.SelectedRows)
                     {
                         var getBookingId = Convert.ToInt32(item.Cells[5].Value);
@@ -126,11 +147,13 @@ namespace Session2
                                             join y in context.Packages on x.packageIdFK equals y.packageId
                                             select y.packageQuantity).First();
 
+                        //Check if the DB still have enough packages for update
                         if ((checkPackage - Int32.Parse(quantityBox.Text)) < 0)
                         {
                             MessageBox.Show("Cannot update quantity as Package quantity will hit negative!",
                                 "Not enough packages", MessageBoxButtons.OK, MessageBoxIcon.Warning);
                         }
+                        //Else proceed with update and change status back to "Pending" for Manager to approve again
                         else
                         {
                             var UpdateBooking = (from x in context.Bookings
